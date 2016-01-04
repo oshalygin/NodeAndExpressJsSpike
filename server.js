d9663a7d3924e48cbd44df9f1950d6a9e56c9080
oshalygin@gmail.com <oshalygin@gmail.com>
2016-01-03 22:36:01 -0800

    adding bower and a few other config related items

---
+++ server.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let express = require("express");
+
+let app = express();
+app.use(express.static("wwwroot"));
+app.use(express.static("src/views"));
+
+let port = 9999;
+
+
+
+app.get("/", (request, response) => {
+    response.send("Hello World");
+});
+
+app.get("/books", (request, response) => {
+    response.send("Hello World");
+});
+
+app.listen(port, (error) => {
+    console.log("Listening on Port: " + port);
+});
