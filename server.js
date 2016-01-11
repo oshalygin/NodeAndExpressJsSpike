c6a6799b5f4a06829c62973516321e4b760305ff
oshalygin@gmail.com <oshalygin@gmail.com>
2016-01-10 21:33:36 -0800

    aadded ejs

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 "use strict";
 
 let express = require("express");
-let handlebars = require("express-handlebars");
+
 let app = express();
 app.use(express.static("wwwroot"));
 // app.use(express.static("src/views"));
 
 
 app.set("views", "./src/views");
-app.engine(".hbs", handlebars({ extname: ".hbs" }));
-
-app.set("view engine", ".hbs");
+app.set("view engine", "ejs");
 
 let port = process.env.PORT || 9999;
 
 app.get("/", (request, response) => {
-    response.render("index");
+    response.render("index", {title: "Derping from handlebars rendering", list: ["a", "b"] });
 });
 
 app.get("/books", (request, response) => {
